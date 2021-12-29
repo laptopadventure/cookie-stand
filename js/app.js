a96f97b7a13d118060918209cbdfcb216c35e7fd
laptopadventure <bazelart13@gmail.com>
2021-12-28 21:29:45 -0800

    moves lists to tables, literals to constructors

--- js/app.js
+++ js/app.js
@@ -16,262 +16,126 @@ function formatTime (hour) {
     hour -= 12; //13 o'clock > 1 o'clock
     meridiem = 'pm';
   }
-  return `${hour}${meridiem}`; //"1pm"
+  return `${hour}:00${meridiem}`; //"1:00pm"
 }
 
-//data
+///returns an empty array with each index as an hour in the day set to zero.
+function cleanTotalsArray() {
+  let newTotalsArray = [];
+  for(let hourIndex = openingHour; hourIndex <= closingHour; hourIndex++) {
+    newTotalsArray.push(0);
+  }
+  return newTotalsArray;
+}
 
-const seattle = {
-  name: 'Seattle',
-  min: 23,
-  max: 65,
-  avgCookiesPerSale: 6.3,
-  simulatedResults: [],
-  simulatedTotal: 0,
-  getRandomCustomerAmt: function() {
-    return Math.ceil(Math.random() * (this.max - this.min + 1) + this.min);
-  },
-  //processes one "day", setting the simulatedResults and simulatedTotal
-  simulate: function() {
-    ///final product, we replace simulatedResults with this for the location
+///global list of all storelocations
+const stores = [];
+///global list of how many cookies earned at a certain hour, in the future this will need to be flushed again when re-simulation happens.
+const totalsEachHour = cleanTotalsArray();
+///and how many we make in a single day
+let totalsEachDay = 0;
+
+function StoreLocation(name, min, max, avg) {
+  /// Name of the store
+  this.name = name;
+  /// Minimum customers that can visit in an hour
+  this.min = min;
+  /// Maximum customers that can visit in an hour
+  this.max = max;
+  /// Average cookies a single customer will buy.
+  this.avg = avg;
+  this.simulate = function() {
     let simulatedDay = [];
-    ///total amount generated
     let total = 0;
-    for(let hourIndex = openingHour; hourIndex <= closingHour; hourIndex++) {
-      let customersThisHour = this.getRandomCustomerAmt();
+    for(let hour = openingHour; hour <= closingHour; hour++) {
+      let customersThisHour = Math.ceil(Math.random() * (this.max - this.min + 1) + this.min);
       //don't forget ceil, avgCookiesPerSale has decimals and you can't buy half a cookie so we assume 0.5 equals an extra cookie
-      let salesThisHour = Math.ceil(customersThisHour * this.avgCookiesPerSale);
+      let salesThisHour = Math.ceil(customersThisHour * this.avg);
       total += salesThisHour;
+      // console.log(totalsEachHour[hour-o])
+      totalsEachHour[hour - openingHour] += salesThisHour;
       simulatedDay.push(salesThisHour);
     }
     this.simulatedResults = simulatedDay;
-    this.simulatedTotal = total;
-  },
-  render: function() {
-    this.simulate();
-    ///place in the document ALL location data sits
-    let locationStatsElement = document.getElementById('location_stats');
-    ///section we put all THIS SPECIFIC location's data inside
-    let section = locationStatsElement.appendChild(document.createElement('section'));
-    section.id = this.name.toLowerCase();
-    //title this section
-    let h3 = section.appendChild(document.createElement('h3'));
-    h3.textContent = this.name;
-    //make a new list and append
-    let ul = section.appendChild(document.createElement('ul'));
-    for(let hour = openingHour; hour <= closingHour; hour++) {
-      let li = ul.appendChild(document.createElement('li'));
-      li.textContent = `${formatTime(hour)}: ${this.simulatedResults[hour - openingHour]}`;
-    }
-    //finally add the total
-    let li = ul.appendChild(document.createElement('li'));
-    li.textContent = `Total: ${this.simulatedTotal}`;
-  }
-};
+    this.total = total;
+    totalsEachDay += total;
+  };
+  this.render = function() {
+    let locationTableBody = document.querySelector('tbody');
+    //new row
+    let locationRow = locationTableBody.appendChild(document.createElement('tr'));
 
-const tokyo = {
-  name: 'Tokyo',
-  min: 3,
-  max: 24,
-  avgCookiesPerSale: 1.2,
-  simulatedResults: [],
-  simulatedTotal: 0,
-  getRandomCustomerAmt: function() {
-    return Math.ceil(Math.random() * (this.max - this.min + 1) + this.min);
-  },
-  //processes one "day", setting the simulatedResults and simulatedTotal
-  simulate: function() {
-    ///final product, we replace simulatedResults with this for the location
-    let simulatedDay = [];
-    ///total amount generated
-    let total = 0;
-    for(let hourIndex = openingHour; hourIndex <= closingHour; hourIndex++) {
-      let customersThisHour = this.getRandomCustomerAmt();
-      //don't forget ceil, avgCookiesPerSale has decimals and you can't buy half a cookie so we assume 0.5 equals an extra cookie
-      let salesThisHour = Math.ceil(customersThisHour * this.avgCookiesPerSale);
-      total += salesThisHour;
-      simulatedDay.push(salesThisHour);
-    }
-    this.simulatedResults = simulatedDay;
-    this.simulatedTotal = total;
-  },
-  render: function() {
-    this.simulate();
-    ///place in the document ALL location data sits
-    let locationStatsElement = document.getElementById('location_stats');
-    ///section we put all THIS SPECIFIC location's data inside
-    let section = locationStatsElement.appendChild(document.createElement('section'));
-    section.id = this.name.toLowerCase();
-    //title this section
-    let h3 = section.appendChild(document.createElement('h3'));
-    h3.textContent = this.name;
-    //make a new list and append
-    let ul = section.appendChild(document.createElement('ul'));
-    for(let hour = openingHour; hour <= closingHour; hour++) {
-      let li = ul.appendChild(document.createElement('li'));
-      li.textContent = `${formatTime(hour)}: ${this.simulatedResults[hour - openingHour]}`;
-    }
-    //finally add the total
-    let li = ul.appendChild(document.createElement('li'));
-    li.textContent = `Total: ${this.simulatedTotal}`;
-  }
-};
+    //first item: the name
+    let thName = locationRow.appendChild(document.createElement('th'));
+    thName.textContent = this.name;
+    // tdName.classList.add(this.name.toLowerCase());
 
-const dubai = {
-  name: 'Dubai',
-  min: 11,
-  max: 38,
-  avgCookiesPerSale: 3.7,
-  simulatedResults: [],
-  simulatedTotal: 0,
-  getRandomCustomerAmt: function() {
-    return Math.ceil(Math.random() * (this.max - this.min + 1) + this.min);
-  },
-  //processes one "day", setting the simulatedResults and simulatedTotal
-  simulate: function() {
-    ///final product, we replace simulatedResults with this for the location
-    let simulatedDay = [];
-    ///total amount generated
-    let total = 0;
+    //the next items: the cookie stats minus total
     for(let hourIndex = openingHour; hourIndex <= closingHour; hourIndex++) {
-      let customersThisHour = this.getRandomCustomerAmt();
-      //don't forget ceil, avgCookiesPerSale has decimals and you can't buy half a cookie so we assume 0.5 equals an extra cookie
-      let salesThisHour = Math.ceil(customersThisHour * this.avgCookiesPerSale);
-      total += salesThisHour;
-      simulatedDay.push(salesThisHour);
+      let td = locationRow.appendChild(document.createElement('td'));
+      td.textContent = this.simulatedResults[hourIndex - openingHour];
+      td.classList.add(this.name.toLowerCase());
     }
-    this.simulatedResults = simulatedDay;
-    this.simulatedTotal = total;
-  },
-  render: function() {
-    this.simulate();
-    ///place in the document ALL location data sits
-    let locationStatsElement = document.getElementById('location_stats');
-    ///section we put all THIS SPECIFIC location's data inside
-    let section = locationStatsElement.appendChild(document.createElement('section'));
-    section.id = this.name.toLowerCase();
-    //title this section
-    let h3 = section.appendChild(document.createElement('h3'));
-    h3.textContent = this.name;
-    //make a new list and append
-    let ul = section.appendChild(document.createElement('ul'));
-    for(let hour = openingHour; hour <= closingHour; hour++) {
-      let li = ul.appendChild(document.createElement('li'));
-      li.textContent = `${formatTime(hour)}: ${this.simulatedResults[hour - openingHour]}`;
-    }
-    //finally add the total
-    let li = ul.appendChild(document.createElement('li'));
-    li.textContent = `Total: ${this.simulatedTotal}`;
-  }
-};
 
-const paris = {
-  name: 'Paris',
-  min: 20,
-  max: 38,
-  avgCookiesPerSale: 2.3,
-  simulatedResults: [],
-  simulatedTotal: 0,
-  getRandomCustomerAmt: function() {
-    return Math.ceil(Math.random() * (this.max - this.min + 1) + this.min);
-  },
-  //processes one "day", setting the simulatedResults and simulatedTotal
-  simulate: function() {
-    ///final product, we replace simulatedResults with this for the location
-    let simulatedDay = [];
-    ///total amount generated
-    let total = 0;
-    for(let hourIndex = openingHour; hourIndex <= closingHour; hourIndex++) {
-      let customersThisHour = this.getRandomCustomerAmt();
-      //don't forget ceil, avgCookiesPerSale has decimals and you can't buy half a cookie so we assume 0.5 equals an extra cookie
-      let salesThisHour = Math.ceil(customersThisHour * this.avgCookiesPerSale);
-      total += salesThisHour;
-      simulatedDay.push(salesThisHour);
-    }
-    this.simulatedResults = simulatedDay;
-    this.simulatedTotal = total;
-  },
-  render: function() {
-    this.simulate();
-    ///place in the document ALL location data sits
-    let locationStatsElement = document.getElementById('location_stats');
-    ///section we put all THIS SPECIFIC location's data inside
-    let section = locationStatsElement.appendChild(document.createElement('section'));
-    section.id = this.name.toLowerCase();
-    //title this section
-    let h3 = section.appendChild(document.createElement('h3'));
-    h3.textContent = this.name;
-    //make a new list and append
-    let ul = section.appendChild(document.createElement('ul'));
-    for(let hour = openingHour; hour <= closingHour; hour++) {
-      let li = ul.appendChild(document.createElement('li'));
-      li.textContent = `${formatTime(hour)}: ${this.simulatedResults[hour - openingHour]}`;
-    }
-    //finally add the total
-    let li = ul.appendChild(document.createElement('li'));
-    li.textContent = `Total: ${this.simulatedTotal}`;
-  }
-};
+    //the last item: the total cookies at this location
+    let tdTotal = locationRow.appendChild(document.createElement('td'));
+    tdTotal.textContent = this.total;
+    tdTotal.classList.add(this.name.toLowerCase());
+  };
+  //there really isn't a reason you'd want an unsimulated StoreLocation, so we just go ahead and do that on init
+  this.simulate();
+  //and finally add ourselves to the global list of all stores
+  stores.push(this);
+}
 
-const lima = {
-  name: 'Lima',
-  min: 1,
-  max: 16,
-  avgCookiesPerSale: 4.6,
-  simulatedResults: [],
-  simulatedTotal: 0,
-  //returns a random amount of customers that could visit in an hour
-  getRandomCustomerAmt: function() {
-    return Math.ceil(Math.random() * (this.max - this.min + 1) + this.min);
-  },
-  //processes one "day", setting the simulatedResults and simulatedTotal
-  simulate: function() {
-    ///final product, we replace simulatedResults with this for the location
-    let simulatedDay = [];
-    ///total amount generated
-    let total = 0;
-    for(let hourIndex = openingHour; hourIndex <= closingHour; hourIndex++) {
-      let customersThisHour = this.getRandomCustomerAmt();
-      //don't forget ceil, avgCookiesPerSale has decimals and you can't buy half a cookie so we assume 0.5 equals an extra cookie
-      let salesThisHour = Math.ceil(customersThisHour * this.avgCookiesPerSale);
-      total += salesThisHour;
-      simulatedDay.push(salesThisHour);
-    }
-    this.simulatedResults = simulatedDay;
-    this.simulatedTotal = total;
-  },
-  render: function() {
-    this.simulate();
-    ///place in the document ALL location data sits
-    let locationStatsElement = document.getElementById('location_stats');
-    ///section we put all THIS SPECIFIC location's data inside
-    let section = locationStatsElement.appendChild(document.createElement('section'));
-    section.id = this.name.toLowerCase();
-    //title this section
-    let h3 = section.appendChild(document.createElement('h3'));
-    h3.textContent = this.name;
-    //make a new list and append
-    let ul = section.appendChild(document.createElement('ul'));
-    for(let hour = openingHour; hour <= closingHour; hour++) {
-      let li = ul.appendChild(document.createElement('li'));
-      li.textContent = `${formatTime(hour)}: ${this.simulatedResults[hour - openingHour]}`;
-    }
-    //finally add the total
-    let li = ul.appendChild(document.createElement('li'));
-    li.textContent = `Total: ${this.simulatedTotal}`;
-  }
-};
+//instantiate our objects
+
+new StoreLocation('Seattle', 23, 65, 6.3);
+new StoreLocation('Tokyo', 3, 24, 1.2);
+new StoreLocation('Dubai', 11, 38, 3.7);
+new StoreLocation('Paris', 20, 38, 2.3);
+new StoreLocation('Lima', 1, 16, 4.6);
 
 ///document modification - data has been prepared by this point, we just need to add it in.
 
-///creates the document results for one location as an unordered list
+///Sets up the location data table's header section.
+function renderTable() {
+  let table = document.querySelector('table');
+  let thead = table.appendChild(document.createElement('thead'));
+  let theadRow = thead.appendChild(document.createElement('tr'));
+  //empty spot as the top left corner.
+  theadRow.appendChild(document.createElement('th'));
+  //now all the formatted hous
+  for(let hour = openingHour; hour <= closingHour; hour++) {
+    let th = theadRow.appendChild(document.createElement('th'));
+    th.textContent = formatTime(hour);
+  }
+  //now one for total
+  let thTotal = theadRow.appendChild(document.createElement('th'));
+  thTotal.textContent = 'Daily Location Total';
+}
 
-seattle.render();
-tokyo.render();
-dubai.render();
-paris.render();
-lima.render();
+///Must be called after rendering each StoreLocation
+function renderHourlyTotalsRow() {
+  let tbody = document.querySelector('tbody');
+  let tr = tbody.appendChild(document.createElement('tr'));
+  //name of this row
+  let th = tr.appendChild(document.createElement('th'));
+  th.textContent = 'Totals';
+  //totals for each hour
+  for(let hourIndex = openingHour; hourIndex <= closingHour; hourIndex++) {
+    let td = tr.appendChild(document.createElement('td'));
+    td.textContent = totalsEachHour[hourIndex - openingHour];
+    td.classList.add('totals');
+  }
+  //total of all totals
+  let td = tr.appendChild(document.createElement('td'));
+  td.textContent = totalsEachDay;
+  td.classList.add('totals');
+}
 
-//possible improvements:
-//remove copy pasting the same function for each object
-//make a list of each location object and iterate through that for simulating results and adding each location to the document
+renderTable();
+for(let storeIndex = 0; storeIndex < stores.length; storeIndex++) {
+  stores[storeIndex].render();
+}
+renderHourlyTotalsRow();
